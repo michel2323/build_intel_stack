f3f1a4dc35855abb74e9a0ab94f9a5e1698e857d
FirstName LastName <your@email.com>
2025-07-24 08:21:44 -0500

    Update

--- build_neo.sh
+++ build_neo.sh
@@ -1,4 +1,4 @@
- #!/bin/bash
+#!/bin/bash
 
 # Set prefix to current working directory (absolute path)
 prefix=$(pwd)
@@ -10,7 +10,7 @@ cd compute-runtime
 git revert 18c25e5aa3fc00c7d47469713adeace08a9aec07
 
 # work around compilation failures
-## already defined in gmmlib
+# already defined in gmmlib
 sed -i '/__stdcall/d' shared/source/gmm_helper/gmm_lib.h
 ## extend LD_LIBRARY_PATH, don't overwrite it
 # find . \( -name CMakeLists.txt -or -name '*.cmake' \) -exec \
@@ -36,7 +36,7 @@ CMAKE_FLAGS+=(-DCMAKE_INSTALL_PREFIX=${prefix})
 CMAKE_FLAGS+=(-DCMAKE_CROSSCOMPILING:BOOL=OFF)
 
 # Explicitly use our cmake toolchain file
-CMAKE_FLAGS+=(-DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN})
+# CMAKE_FLAGS+=(-DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN})
 
 # Don't run tests
 CMAKE_FLAGS+=(-DSKIP_UNIT_TESTS:Bool=true)
@@ -49,7 +49,9 @@ CMAKE_FLAGS+=(-DNEO_ENABLE_i915_PRELIM_DETECTION=TRUE)
 
 # libigc installs libraries and pkgconfig rules in lib64, so look for them there.
 # FIXME: shouldn't BinaryBuilder do this?
+export PATH=${prefix}/bin:$PATH
+export LD_LIBRARY_PATH=${prefix}/lib64:${prefix}/lib
 export PKG_CONFIG_PATH=${prefix}/lib64/pkgconfig:${prefix}/lib/pkgconfig
 
 cmake -B build -S . -GNinja ${CMAKE_FLAGS[@]}
-ninja -C build -j $(nproc) install"""
+ninja -C build -j $(nproc) install
